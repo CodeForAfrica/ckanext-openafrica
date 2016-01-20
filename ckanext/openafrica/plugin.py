b567580bac41bef976732e5571da0fb9378405ad
Jude Mwenda <judemwenda@gmail.com>
2016-01-20 15:19:27 -0500

    Base work from data.mx theme

--- ckanext/openafrica/plugin.py
+++ ckanext/openafrica/plugin.py
@@ -1,7 +1,14 @@
 import ckan.plugins as plugins
+import ckan.plugins.toolkit as toolkit
+
 
 class OpenAfricaPlugin(plugins.SingletonPlugin):
     '''OpenAfrica templating plugin done in 2015.
 
     '''
-    pass
+    plugins.implements(plugins.IConfigurer)
+
+    def update_config(self, config):
+        toolkit.add_template_directory(config, 'templates')
+    
+        toolkit.add_public_directory(config, 'public')
