0c604cf13daf721b0e75a2a1575e4066cf809f47
Jude Mwenda <judemwenda@gmail.com>
2016-02-08 01:40:15 -0500

    Changes to the extra url paths

--- ckanext/openafrica/plugin.py
+++ ckanext/openafrica/plugin.py
@@ -7,9 +7,27 @@ class OpenAfricaPlugin(plugins.SingletonPlugin):
 
     '''
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IRoutes, inherit=True)
 
     def update_config(self, config):
         toolkit.add_template_directory(config, 'templates')
     
         toolkit.add_public_directory(config, 'public')
         toolkit.add_resource('fanstatic', 'openafrica')
+
+    def before_map(self, map):
+        map.connect('/about/terms-and-conditions',
+                    controller='ckanext.openafrica.controller:CustomPageController', action='toc')
+        map.connect('/about/accessibility',
+                    controller='ckanext.openafrica.controller:CustomPageController', action='accessibility')
+        map.connect('/about/code-of-conduct',
+                    controller='ckanext.openafrica.controller:CustomPageController', action='coc')
+        map.connect('/about/moderation-policy',
+                    controller='ckanext.openafrica.controller:CustomPageController', action='moderation')
+        map.connect('/about/faq',
+                    controller='ckanext.openafrica.controller:CustomPageController', action='faq')
+        map.connect('/about/privacy',
+                    controller='ckanext.openafrica.controller:CustomPageController', action='privacy')
+        map.connect('/about/contact-us',
+                    controller='ckanext.openafrica.controller:CustomPageController', action='contact')
+        return map
