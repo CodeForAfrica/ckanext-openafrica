a61bd3db9de0efc0b48340fdb6d3eaeea8455539
Nnenanya Obinna <obinna.nnenanya@andela.com>
2017-09-11 16:50:40 +0100

    display button only when datarequests is installed

--- ckanext/openafrica/lib/helpers.py
+++ ckanext/openafrica/lib/helpers.py
@@ -1,5 +1,14 @@
 import datetime
+from ckan.common import config
 
 def current_year():
     year = datetime.datetime.now().year
-    return year    
+    return year
+
+def enable_datarequest():
+    config_plugins = config.get('ckan.plugins', None)
+    if ' datarequests ' in config_plugins:
+        return True
+    else:
+        return False
+
