494710c1852110ad6c7ceaf10f602396df6dfcc6
Nnenanya Obinna <obinna.nnenanya@andela.com>
2017-09-12 12:36:15 +0100

    make the helper function reusable

--- ckanext/openafrica/lib/helpers.py
+++ ckanext/openafrica/lib/helpers.py
@@ -5,10 +5,17 @@ def current_year():
     year = datetime.datetime.now().year
     return year
 
-def enable_datarequest():
-    config_plugins = config.get('ckan.plugins', None)
-    if ' datarequests ' in config_plugins:
+def is_plugin_enabled(plugin_name):
+    '''Returns a boolean True or False, indicating if the supplied plugin_name
+    is enabled in the config file
+
+    :param plugin_name: the name of the plugin to check for in the config file
+    :type plugin_name: string
+    '''
+    config_plugins = config.get('ckan.plugins', '')
+    config_plugins = ' ' + config_plugins + ' '
+    plugin_name = ' ' + plugin_name + ' '
+    if plugin_name in config_plugins:
         return True
     else:
         return False
-
