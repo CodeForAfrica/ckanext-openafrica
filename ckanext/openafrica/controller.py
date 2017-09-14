fafc87d021140636a60904086a95dcbe64ee6af5
Nnenanya Obinna <obinna.nnenanya@andela.com>
2017-09-14 16:16:53 +0100

    add licence statements to controllers

--- ckanext/openafrica/controller.py
+++ ckanext/openafrica/controller.py
@@ -1,4 +1,21 @@
+# encoding: utf-8
 
+# Copyright (c) 2015-2017 Code for Africa
+
+# This file is part of CKAN openAfrica Extension.
+
+# CKAN openAfrica Extension is free software: you can redistribute it and/or
+# modify it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+
+# CKAN openAfrica Extension is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with CKAN openAfrica Extension. If not, see <http://www.gnu.org/licenses/>.
 
 from ckan.lib.base import render
 
