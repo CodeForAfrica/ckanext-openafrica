2034861cc2a2923ece9b130e1ddf961bcd74eb02
David Lemayian <dlemayian@gmail.com>
2017-09-18 21:11:53 +0300

    Update helpers.py

--- ckanext/openafrica/lib/helpers.py
+++ ckanext/openafrica/lib/helpers.py
@@ -2,14 +2,14 @@
 
 # Copyright (c) 2015-2017 Code for Africa
 
-# This file is part of CKAN openAfrica Extension.
+# This file is part of CKAN openAFRICA Extension.
 
-# CKAN openAfrica Extension is free software: you can redistribute it and/or
+# CKAN openAFRICA Extension is free software: you can redistribute it and/or
 # modify it under the terms of the GNU Affero General Public License as published by
 # the Free Software Foundation, either version 2 of the License, or
 # (at your option) any later version.
 
-# CKAN openAfrica Extension is distributed in the hope that it will be useful,
+# CKAN openAFRICA Extension is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero General Public License for more details.
