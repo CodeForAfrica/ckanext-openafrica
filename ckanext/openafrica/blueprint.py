90d65673e284e98718e360819e9efcbd545444ef
Xavier Frankline <xf.xavierfrank@gmail.com>
2025-01-08 15:38:12 +0300

    render robot txt as txt type

--- ckanext/openafrica/blueprint.py
+++ ckanext/openafrica/blueprint.py
@@ -11,7 +11,7 @@ def static_path(path):
 
 def robots_txt():
     '''display robots.txt'''
-    resp = make_response(render('home/robots.txt'))
+    resp = make_response(render('robots.txt'))
     resp.headers['Content-Type'] = "text/plain; charset=utf-8"
     return resp
 
